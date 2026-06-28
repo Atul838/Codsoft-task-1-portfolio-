bf90389ee53d9f5f0c0a5b3d404399a5c67e3895
Atul838 <tanispal7@gmail.com>
2026-06-28 15:42:06 +0530

    Delete landingpage.js

--- landingpage.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    alert("Welcome To TechZone Landing Page");
-};
-
-document.getElementById("startBtn").addEventListener("click", function(){
-
-    alert("Thank You For Visiting TechZone!");
-
-});
