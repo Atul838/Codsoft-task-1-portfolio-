aad0df80110f347145bbaa45cbdf702a8a564942
Atul838 <tanispal7@gmail.com>
2026-06-28 14:35:20 +0530

    Create landingpage.js

---
+++ landingpage.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    alert("Welcome To TechZone Landing Page");
+};
+
+document.getElementById("startBtn").addEventListener("click", function(){
+
+    alert("Thank You For Visiting TechZone!");
+
+});
